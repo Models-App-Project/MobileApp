6a1bdd9bc9cf45da1ff663572dd3e63d604943d2
João Victor <jvflima14@gmail.com>
2024-09-29 12:18:01 -0300

    feat(textStyles): Adicionando folder para Text Styles ao projeto e aplicando as Fonts Family para os estilos de texto

---
+++ lib/common/constant/app_text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle bigText = TextStyle(
+      fontFamily: 'Crimson Pro',
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      letterSpacing: 3);
+
+  static const TextStyle regularText = TextStyle(
+      fontFamily: 'Crimson Pro', fontSize: 16, fontWeight: FontWeight.w400);
+
+  static const TextStyle smallText = TextStyle(
+      fontFamily: 'Crimson Pro', fontSize: 14, fontWeight: FontWeight.w300);
+
+  static const TextStyle buttonText = TextStyle(
+      fontFamily: 'Bree Serif', fontSize: 16, fontWeight: FontWeight.w400);
+}
