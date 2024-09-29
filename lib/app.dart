43ac2da172fae080ffe4d7c608fc8d7f1186968f
João Victor <jvflima14@gmail.com>
2024-09-29 12:16:40 -0300

    feat(app): Adicionando ocultação da barra de status

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/features/splash/splash_page.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/features/home/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky,
+        overlays: []);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
