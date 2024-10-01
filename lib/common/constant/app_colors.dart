3775ade414a0b2a3d1d8a5452d9ff134300aa2e5
AnaSouzaPinheiro <anapinheirole@gmail.com>
2024-09-30 21:14:47 -0300

    style (app_colors.dart): adição de constantes de cor para hintText, borda do input e cursor

--- lib/common/constant/app_colors.dart
+++ lib/common/constant/app_colors.dart
@@ -6,4 +6,14 @@ class AppColors {
   static const Color white = Colors.white;
 
   static const Color buttonBlue = Color(0xFF1A3568);
+
+  static const TextStyle hintText = TextStyle(
+    color: Color(0xFFC1C0C1),
+  );
+
+  static const borderInput = Color(0xFFC1C0C1);
+
+  static const focusedInput = Color(0xFF4F6BFF);
+
+  static const cursor = Color(0xFF565656);
 }
