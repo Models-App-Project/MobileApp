bcdbcac826906af12e2da1b8c2819244cb4f0ad5
João Victor <jvflima14@gmail.com>
2024-10-05 17:11:49 -0300

    refactor(App): Refatorando código, utilizando a initial page como body inicial da aplicação

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_1/features/forms/forms_page.dart';
-import 'package:flutter_application_1/features/home/home_page.dart';
+import 'package:flutter_application_1/features/inital.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,7 +11,7 @@ class App extends StatelessWidget {
         overlays: []);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FormsPage(),
+      home: Initial(),
     );
   }
 }
