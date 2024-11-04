845faaacd653fed4f4e9fa6be1722f90c3897bfd
João Victor <jvflima14@gmail.com>
2024-11-04 19:10:08 -0300

    feat(AppTextStyles): Adding a constant text

--- lib/common/constant/app_text_styles.dart
+++ lib/common/constant/app_text_styles.dart
@@ -12,6 +12,9 @@ class AppTextStyles {
   static const TextStyle regularText = TextStyle(
       fontFamily: 'Crimson Pro', fontSize: 16, fontWeight: FontWeight.w400);
 
+  static const TextStyle regularTextSerif = TextStyle(
+      fontFamily: 'Bree Serif', fontSize: 14, fontWeight: FontWeight.w400);
+
   static const TextStyle smallText = TextStyle(
       fontFamily: 'Crimson Pro', fontSize: 14, fontWeight: FontWeight.w300);
 
@@ -25,5 +28,8 @@ class AppTextStyles {
       fontFamily: 'Bree Serif', fontSize: 12, fontWeight: FontWeight.w400);
 
   static const TextStyle buttonFormsText = TextStyle(
-      fontFamily: 'Josefin Sans', fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 1.4);
+      fontFamily: 'Josefin Sans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.4);
 }
