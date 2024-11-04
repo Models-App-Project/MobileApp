d70e3ed2aaf2b67bc76001ebb75edf47d1205ad1
João Victor <jvflima14@gmail.com>
2024-11-04 19:12:31 -0300

    feat(FormsPage): Adding the focus node for Forms Page to hide the UI status bar

--- lib/features/forms/forms_page.dart
+++ lib/features/forms/forms_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/common/constant/app_colors.dart';
 import 'package:flutter_application_1/common/widgets/custom_forms_button.dart';
 import 'package:flutter_application_1/common/widgets/custom_description_field.dart';
@@ -9,39 +10,79 @@ class FormsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Criação de FocusNode para cada campo de texto
+    final focusNodes = List<FocusNode>.generate(10, (_) => FocusNode());
+
+    // Função para ativar o modo imersivo
+    void _enableImmersiveMode() {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    }
+
+    // Função para esconder a barra de status quando o foco sai de um campo
+    void _addFocusListeners() {
+      for (var focusNode in focusNodes) {
+        focusNode.addListener(() {
+          if (!focusNode.hasFocus) {
+            _enableImmersiveMode();
+          }
+        });
+      }
+    }
+
+    // Inicializa os listeners para foco
+    _addFocusListeners();
+
     return Scaffold(
       backgroundColor: AppColors.white,
       body: Column(
         children: [
-          // A imagem que fica fixa na parte superior
-          Image.asset('assets/images/logo_fernanda2.png'),
-          // O conteúdo rolável embaixo da imagem
+          const SizedBox(
+            height: 20,
+          ),
+          Stack(
+            children: [
+              // A imagem que fica fixa na parte superior
+              Image.asset('assets/images/logo_fernanda2.png'),
+              // Botão de voltar posicionado acima da imagem
+              Positioned(
+                top: 10, // Ajuste conforme necessário para a posição vertical
+                left: 10, // Ajuste conforme necessário para a posição horizontal
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.black),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+          // O conteúdo rolável abaixo da imagem
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              children: const [
-                SizedBox(height: 20),
-                CustomTextField(campo: 'Name', hint: 'Full Name'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Age', hint: 'Age'),
-                SizedBox(height: 16),
-                CustomDescriptionField(),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Email', hint: 'Email (example@gmail.com)'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Height', hint: 'Height (cm)'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Eye’s Color', hint: 'Eye’s Color'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Hair’s Color', hint: 'Hair’s Color'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Waistline', hint: 'Waist (cm)'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Bust', hint: 'Bust (cm)'),
-                SizedBox(height: 16),
-                CustomTextField(campo: 'Hip', hint: 'Hip (cm)'),
-                SizedBox(height: 16),
-                Row(
+              children: [
+                const SizedBox(height: 20),
+                CustomTextField(campo: 'Name', hint: 'Full Name', focusNode: focusNodes[0]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Age', hint: 'Age', focusNode: focusNodes[1]),
+                const SizedBox(height: 16),
+                const CustomDescriptionField(),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Email', hint: 'Email (example@gmail.com)', focusNode: focusNodes[2]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Height', hint: 'Height (cm)', focusNode: focusNodes[3]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Eye’s Color', hint: 'Eye’s Color', focusNode: focusNodes[4]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Hair’s Color', hint: 'Hair’s Color', focusNode: focusNodes[5]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Waistline', hint: 'Waist (cm)', focusNode: focusNodes[6]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Bust', hint: 'Bust (cm)', focusNode: focusNodes[7]),
+                const SizedBox(height: 16),
+                CustomTextField(campo: 'Hip', hint: 'Hip (cm)', focusNode: focusNodes[8]),
+                const SizedBox(height: 16),
+                const Row(
                   children: [
                     CustomFormsButton(
                       title: 'CANCEL REQUEST',
@@ -56,7 +97,7 @@ class FormsPage extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 32),
+                const SizedBox(height: 32),
               ],
             ),
           ),
